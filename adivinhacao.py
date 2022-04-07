38a786230ebe5c5bfde1744d5a190685623f5d26
Elson <elsonsaints@gmail.com>
2022-04-07 03:42:51 -0300

    inicio da estrutura de dados

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,10 +1,23 @@
 print("*********************")
 print("**** Bem Vindo! *****")
 print("*********************")
+
 numero_secreto = 31
-chute = input("Digite um numero: ")
-print("\nvocê digitou", chute)
-if numero_secreto == chute:
-    print("você acertou!")
-else:
-    print("você errou!")
+total_de_tentativas = 3
+for rodada in range(1, total_de_tentativas + 1):
+    chute = int(input("\nDigite um número: "))
+    print("\nvocê digitou", chute)
+
+    acertou = numero_secreto == chute
+    maior   = chute>numero_secreto
+    menor   = chute<numero_secreto
+
+    if (acertou):
+        print("você acertou!")
+    else:
+        if (maior):
+            print("você errou! Seu número foi maior que o numero secreto.")
+        elif (menor):
+            print("você errou! Seu número foi menor que o numero secreto.")
+print("\n>>>> Fim de Jogo! <<<<")
+    
