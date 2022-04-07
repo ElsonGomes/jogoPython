045fd47858038e890b742c99c34a6ea81ca47793
Elson <elsonsaints@gmail.com>
2022-04-07 02:30:14 -0300

    adicionado uma funcionalidade

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,3 +4,7 @@ print("*********************")
 numero_secreto = 31
 chute = input("Digite um numero: ")
 print("\nvocê digitou", chute)
+if numero_secreto == chute:
+    print("você acertou!")
+else:
+    print("você errou!")
