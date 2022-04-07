e5221cb7d8029ba94dce5f24741c1975c7aa28fd
Elson <elsonsaints@gmail.com>
2022-04-07 02:11:26 -0300

    Criando o Jogo

---
+++ adivinhacao.py
@@ -0,0 +1,6 @@
+print("*********************")
+print("**** Bem Vindo! *****")
+print("*********************")
+numero_secreto = 31
+chute = input("Digite um numero: ")
+print("\nvocê digitou", chute)
