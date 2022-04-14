48cff653e9a98954876826fd918053b1a295ca0f
Elson <elsonsaints@gmail.com>
2022-04-14 02:35:35 -0300

    novas funcionalidades

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,23 +1,52 @@
+import random
 print("*********************")
 print("**** Bem Vindo! *****")
 print("*********************")
 
-numero_secreto = 31
+numero_secreto = random.randrange(1,101)
 total_de_tentativas = 3
-for rodada in range(1, total_de_tentativas + 1):
-    chute = int(input("\nDigite um número: "))
-    print("\nvocê digitou", chute)
+pontos = 1000
 
-    acertou = numero_secreto == chute
-    maior   = chute>numero_secreto
-    menor   = chute<numero_secreto
+print("Qual o nivel de dificuldade")
+print("(1) Facil (2) Medio (3) Dificil")
+while(True):
+    nivel = int(input("Defina o nivel: "))
 
-    if (acertou):
-        print("você acertou!")
+    if(nivel==1):
+        total_de_tentativas = 10
+        break
+    elif(nivel==2):
+        total_de_tentativas = 5
+        break
+    elif(nivel==3):
+        total_de_tentativas = 3
+        break
     else:
-        if (maior):
-            print("você errou! Seu número foi maior que o numero secreto.")
-        elif (menor):
-            print("você errou! Seu número foi menor que o numero secreto.")
+        print("Nivel incorreto\n")
+        True
+
+
+for rodada in range(1, total_de_tentativas + 1):
+    print(f"\nTentativa {rodada} de {total_de_tentativas}")
+    chute = int(input("\nDigite um número entre (1 e 100): "))
+    print("\nvocê digitou", chute)
+    if(chute<1 or chute>100):
+        print("Voce digitou um numero errado, o numero deve ser entre (1 e 100)")
+        continue
+    if(chute):
+        acertou = numero_secreto == chute
+        maior   = chute>numero_secreto
+        menor   = chute<numero_secreto
+
+        if (acertou):
+            print(f"você acertou! e fez {pontos}")
+            break
+        else:
+            if (maior):
+                print("você errou! Seu número foi maior que o numero secreto.")
+            elif (menor):
+                print("você errou! Seu número foi menor que o numero secreto.")
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos = pontos - pontos_perdidos
 print("\n>>>> Fim de Jogo! <<<<")
     
